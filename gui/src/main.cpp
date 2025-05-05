2b44c6b71a6776a5a5d2121650b91d386bd4b8d5
Niklas <94556032+chickenmcniggels@users.noreply.github.com>
2025-05-05 16:16:20 +0200

    Update main.cpp

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -1,1 +1,17 @@
+#include <QApplication>
+#include <QMainWindow>
+#include <QLabel>
 
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QMainWindow window;
+    window.setWindowTitle("IntuiCAM");
+
+    QLabel* label = new QLabel("Welcome to IntuiCAM");
+    label->setAlignment(Qt::AlignCenter);
+    window.setCentralWidget(label);
+
+    window.resize(800, 600);
+    window.show();
+    return app.exec();
+}
